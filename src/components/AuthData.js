803b34fcb6c8eca1783cc23d2f2507926dcf48b1
unknown <gidadssworld@gmail.com>
2023-01-13 11:33:54 -1100

    INITIAL COMMIT

---
+++ src/components/AuthData.js
@@ -0,0 +1,7 @@
+export  const AuthData = [
+    { "password": "Ram123", "email": "ram@gmail.com", "id": 11 },
+    { "password": "Shyam123@", "email": "shyam23@gmail.com", "id": 5 },
+    { "password": "John123@", "email": "john@gmail.com", "id": 3 },
+    { "password": "Bob324@", "email": "bob32@gmail.com", "id": 9 },
+    { "password": "gidado", "email": "gidadssworld@gmail.com", "id": 1 }
+]
